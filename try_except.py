99ab64de01a47fd12dba3919f7437a3bdb218b8f
VdovichenkoSergey <vdovi4enko.qa@gmail.com>
2020-02-15 19:18:40 +0200

    Lesson 3: 3rd commit

--- try_except.py
+++ try_except.py
@@ -1,6 +1,27 @@
+
+
+'''
 s = input('enter value: ')
 
 try:
     s = int(s)
 except:
     print('Error')
+'''
+print()
+print('Task 2')
+
+
+while True:
+    x = input('Input some digit: ')
+    try:
+        x = float(x)
+        break
+    except ValueError as e:
+        print('Not a number!!!')
+    else:
+        break
+
+
+print(type(x))
+
