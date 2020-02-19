67d8f3aa694db6c19f851ca5a958dc2faa0f332a
VdovichenkoSergey <vdovi4enko.qa@gmail.com>
2020-02-19 08:16:15 +0200

    HomeWork_3_1 done @elpiankova

--- HomeWork_3_1.py
+++ HomeWork_3_1.py
@@ -1,3 +1,5 @@
+print('method 1')
+print()
 s = input('input some string: ')
 
 try:
@@ -6,5 +8,17 @@ try:
         print(list1[i])
 except IndexError:
     print('your string too small, input string with 3 symbols or more: ')
+print()
 
 
+print('method 2')
+print()
+
+s = input('input some string: ')
+
+while True:
+    try:
+        print(s[2], s[-2], s[:5], s[:-2], s[1::2], s[::2], s[::-1], s[::-2], s[-2::-2], s[-2:0:-1], len(s), sep='\n')
+        break
+    except:
+        s = input('your string too small, input string with 3 symbols or more: ')
