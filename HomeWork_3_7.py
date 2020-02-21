4a4f6423f3fdf998c9994775a6a7c0cde4654db1
VdovichenkoSergey <vdovi4enko.qa@gmail.com>
2020-02-21 15:54:31 +0200

    HomeWork_3_7

---
+++ HomeWork_3_7.py
@@ -0,0 +1,8 @@
+s = '''We are not what we should be!
+We are not what we need to be.
+But at least we are not what we used to be
+(Football Coach)'''
+
+print(s)
+print()
+print('Number of words in the text: ', s.count(' ') + 1)
