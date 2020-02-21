65789301916bb847b0bf48263422a2ff7a9496d8
VdovichenkoSergey <vdovi4enko.qa@gmail.com>
2020-02-21 19:57:47 +0200

    HomeWork_3_7 done @elpiankova

--- HomeWork_3_7.py
+++ HomeWork_3_7.py
@@ -2,7 +2,19 @@ s = '''We are not what we should be!
 We are not what we need to be.
 But at least we are not what we used to be
 (Football Coach)'''
-
+l = s.split()
 print(s)
 print()
-print('Number of words in the text: ', s.count(' ') + 1)
+print(l)
+print()
+
+print('Number of words in the text: ', len(l))
+print()
+
+for i in range(len(l)):
+    l[i] = l[i].strip("!'.','?'(')'")
+print(l)
+print()
+
+l.sort(key=str.lower)
+print(l)
