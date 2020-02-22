619367529ebb46a5ff5510f883e413a716a8a3ef
VdovichenkoSergey <vdovi4enko.qa@gmail.com>
2020-02-22 18:28:59 +0200

    Lesson_4_Functions.py

---
+++ Lesson_4_Functions.py
@@ -0,0 +1,18 @@
+'''def func(*word):
+    return word
+
+x = func(1, 2, 3, 4)
+print(x)
+
+#output - (1, 2, 3, 4)
+
+def func2(*value):
+    l = list(value)
+    l.sort()
+    return l[1]
+
+x = func2(input('input some digits: '))
+#x = func2(5, 3, 4, 10)
+print(x)'''
+
+def func 3(**word)
