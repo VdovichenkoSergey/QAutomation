df001153774b03d3d8af93bf52ddf27ab1fce89a
VdovichenkoSergey <vdovi4enko.qa@gmail.com>
2020-02-25 20:02:34 +0200

    fix 2_6 and 3_1

--- HomeWork_2_6.py
+++ HomeWork_2_6.py
@@ -3,7 +3,7 @@ b = int(input('input second digit: '))
 c = int(input('input 3rd digit: '))
 s = 'digits are same: '
 
-if a == b == c:
+if a == b and b == and c == a:
     print(s, '3')
 elif a == b != c or a != b == c or a == c != b:
     print(s, '2')
--- HomeWork_3_1.py
+++ HomeWork_3_1.py
@@ -4,7 +4,7 @@ s = input('input some string: ')
 
 try:
     list1 = [s[2], s[-2], s[:5], s[:-2], s[1::2], s[::2], s[::-1], s[::-2], s[-2::-2], s[-2:0:-1], len(s)]
-    for i in range(len(list1)):
+    for i in list1:
         print(list1[i])
 except IndexError:
     print('your string too small, input string with 3 symbols or more: ')
