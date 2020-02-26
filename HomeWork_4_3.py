b5fc0331a179cd54d708ac4425ac404e5ea24919
VdovichenkoSergey <vdovi4enko.qa@gmail.com>
2020-02-26 12:19:52 +0200

    Home Work 4 done. @elpiankova

---
+++ HomeWork_4_3.py
@@ -0,0 +1,16 @@
+def SrtOny(a):
+    '''Напишите функцию, которая удаляет все небуквенные символы внутри
+строки'''
+    a = str(a)
+    a1 = ''
+    for i in a:
+        if i.isalpha() == True and i.isascii() == True:
+            a1 = a1 + i
+
+    assert a1.isalpha() is True
+    assert a1.isascii() is True
+    return a1
+
+
+x = SrtOny('q1йцуw 2e3r4;@á')
+print(x)
