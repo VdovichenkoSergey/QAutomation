6f5a0b101506aa0d0bff36094abb2ea3c4d99840
VdovichenkoSergey <vdovi4enko.qa@gmail.com>
2020-03-09 17:03:46 +0200

    some changes

--- test_home_work_3/test_triangle.py
+++ test_home_work_3/test_triangle.py
@@ -5,8 +5,11 @@ from HomeWork_3_6 import triangle
 class TestTriangle(unittest.TestCase):
 
     def triangle_true1(self):
-        res = triangle(1, 2, 2)
-        self.assertFalse(res)
+        a = 1
+        b = 2
+        c = 2
+        res = triangle(a, b, c)
+        self.assertTrue(res)
 
 
 if __name__ == "__main__":
