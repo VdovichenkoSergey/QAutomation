a334a978fe044dc50202e7a7324d7af15c6a919e
unknown <vdovi4enko.qa@gmail.com>
2020-03-14 19:14:56 +0200

    lesson_6_2

--- HTML_get.py
+++ HTML_get.py
@@ -1,4 +1,18 @@
 import requests
+import json
 
-x = requests.get('https://google.com')
-print(x.content)
+r = requests.get('https://httpbin.org/get')
+resp = r.json()
+# resp_body = json.loads(r.text)
+# print(resp)
+# print(resp.keys())
+# print(x.url)
+# print(x.status_code)
+# print(x.headers)
+# print(x.text)
+
+r1 = requests.post('https://httpbin.org/post', data='data')
+print(r1.text)
+
+resp1_body = r1.json()
+print(resp1_body)
